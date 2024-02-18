2fe683e671eab580d92a83315671ec920560f083
AnybodyKnows <evgeniiolavrik@gmail.com>
2024-02-19 00:08:18 +0100

    added some classes

--- ClassModule.py
+++ ClassModule.py
@@ -1,12 +1,47 @@
 from collections import UserDict
 
-class Record:
+class Field:
     def __init__(self, value) -> None:
         self.value = value
     
+    def __str__(self):
+        return self.value
+    
+class Name(Field):
+    pass
+        
+class Phone(Field):
+    def validate_number(self, number):
+        if len(number) != 10:
+            return self.phone == number
+        else:
+            return  
+
+class Record:
+    def __init__(self, name) -> None:
+        self.value = name
+        self.phones = []
+
     def add_phone(self, phone_number):
+        self.phones.append(phone_number)
+
+    def del_phone(self, phone_number):
+        try: self.phones.remove(phone_number)
+        except: pass
+
+    def edit_phone(self, old_number, new_number):
         pass
+
+    def find_phone(self, phone_number):
+        pass    
+
 
 class AddressBook(UserDict):
     def add_record(self, key, value):
         self.data[key]= value
+    
+    def find_record(self, key):
+        pass
+
+    def del_record(self, key):
+        pass
--- test.py
+++ test.py
@@ -1,10 +1,21 @@
 import ClassModule as cm  
 
-a = cm.AddressBook()
-a.add_key("key1", 123)
-a.add_key("key2", 456)
-print (a)
-print (type(a))
+
+fld = cm.Field("jon")
+print(fld)
+
+
+# jon_rec = cm.Record("Jon")
+# jon_rec.add_phone(123)
+# jon_rec.add_phone(456)
+# print(f"{jon_rec.value} {jon_rec.phones}")
+
+
+# a = cm.AddressBook()
+# a.add_record("key1", 123)
+# a.add_record("key2", 456)
+# print (a)
+# print (type(a))
 
 
 
